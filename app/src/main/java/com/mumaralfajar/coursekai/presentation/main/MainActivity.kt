8778fbe2c68d8a4bce04d88f0985bc4772fcd46a
mumaralfajar <kuhaaku@gmail.com>
2022-07-14 23:32:13 +0700

    initial commit

---
+++ app/src/main/java/com/mumaralfajar/coursekai/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mumaralfajar.coursekai.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mumaralfajar.coursekai.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
