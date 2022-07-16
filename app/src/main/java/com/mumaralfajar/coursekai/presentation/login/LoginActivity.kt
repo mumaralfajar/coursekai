4d54693326e9ffac8d95e125a2b1a5389efc41bf
mumaralfajar <kuhaaku@gmail.com>
2022-07-16 13:27:14 +0700

    add login screen

---
+++ app/src/main/java/com/mumaralfajar/coursekai/presentation/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.mumaralfajar.coursekai.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mumaralfajar.coursekai.R
+import com.mumaralfajar.coursekai.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginBinding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+    }
+}
