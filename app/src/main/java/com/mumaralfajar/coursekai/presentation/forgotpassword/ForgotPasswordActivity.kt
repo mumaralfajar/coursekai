184c9a38a6688efd5325ca49800806aa3c1a806a
mumaralfajar <kuhaaku@gmail.com>
2022-07-25 04:12:21 +0700

    add forgot password

--- app/src/main/java/com/mumaralfajar/coursekai/presentation/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/mumaralfajar/coursekai/presentation/forgotpassword/ForgotPasswordActivity.kt
@@ -1,18 +1,33 @@
 package com.mumaralfajar.coursekai.presentation.forgotpassword
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Patterns
+import com.google.firebase.auth.FirebaseAuth
+import com.mumaralfajar.coursekai.R
 import com.mumaralfajar.coursekai.databinding.ActivityForgotPasswordBinding
+import com.mumaralfajar.coursekai.utils.hideSoftKeyboard
+import com.mumaralfajar.coursekai.utils.showDialogError
+import com.mumaralfajar.coursekai.utils.showDialogLoading
+import com.mumaralfajar.coursekai.utils.showDialogSuccess
 import org.jetbrains.anko.toast
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     private lateinit var forgotPasswordBinding: ActivityForgotPasswordBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var dialogLoading: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         forgotPasswordBinding = ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(forgotPasswordBinding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        dialogLoading = showDialogLoading(this)
 
         onAction()
     }
@@ -22,8 +37,50 @@ class ForgotPasswordActivity : AppCompatActivity() {
             btnCloseForgotPassword.setOnClickListener { finish() }
 
             btnForgotPassword.setOnClickListener {
-                toast("Forgot Password")
+                val email = etEmailForgotPassword.text.toString().trim()
+
+                if (checkValidation(email)) {
+                    hideSoftKeyboard(this@ForgotPasswordActivity, forgotPasswordBinding.root)
+                    forgotPasswordToServer(email)
+                }
             }
         }
+    }
+
+    private fun forgotPasswordToServer(email: String) {
+        dialogLoading.show()
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnSuccessListener {
+                dialogLoading.dismiss()
+                val dialogSuccess = showDialogSuccess(this, getString(R.string.success_forgot_pass))
+                dialogSuccess.show()
+
+                Handler(Looper.getMainLooper())
+                    .postDelayed({
+                        dialogSuccess.dismiss()
+                        finish()
+                    }, 1500)
+            }
+            .addOnFailureListener {
+                dialogLoading.dismiss()
+                showDialogError(this, it.message.toString())
+            }
+    }
+
+    private fun checkValidation(email: String): Boolean {
+        forgotPasswordBinding.apply {
+            when {
+                email.isEmpty() -> {
+                    etEmailForgotPassword.error = getString(R.string.please_field_your_email)
+                    etEmailForgotPassword.requestFocus()
+                }
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                    etEmailForgotPassword.error = getString(R.string.please_use_valid_email)
+                    etEmailForgotPassword.requestFocus()
+                }
+                else -> return true
+            }
+        }
+        return false
     }
 }
